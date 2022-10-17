1451f46ad5e10659da127d6317f6e2a44a495298
muinalautaro16 <muinalautaro@gmail.com>
2022-10-17 15:31:47 -0300

    first commit

---
+++ js/nuevocliente.js
@@ -0,0 +1,71 @@
+(function(){
+    let DB;
+    const formulario = document.querySelector('#formulario');
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        conectarDB();
+
+        formulario.addEventListener('submit', validarCliente);
+
+        
+    });
+
+    function conectarDB() {
+        const abrirConexion = window.indexedDB.open('crm', 1);
+
+        abrirConexion.onerror = function() {
+            console.log('Hubo un error');
+        };
+
+        abrirConexion.onsuccess = function() {
+            DB = abrirConexion.result;
+        };
+
+    }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        // Validar todos los inputs
+        const nombre = document.querySelector('#nombre').Value;
+        const email = document.querySelector('#email').Value;
+        const telefono = document.querySelector('#telefono').Value;
+        const empresa = document.querySelector('#empresa').Value;
+        
+        if(nombre === '' || email === '' || telefono === '' || empresa === '' ) {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+
+            return;
+        }
+        console.log(validarCliente);
+    }
+
+    function imprimirAlerta(mensaje, tipo) {
+        const alerta = document.querySelector('.alerta');
+
+        if(!alerta) {
+            // Crar la alerta
+            const divMensaje = document.createElement('div');
+            divMensaje.classLists.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border', 'alerta');
+
+            if(tipo === 'error') {
+                divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+            } else {
+                divMensaje.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
+            }
+
+
+            divMensaje.textContent = mensaje;
+
+            formulario.appendChild(divMensaje);
+ 
+            setTimeout(() => {
+                divMensaje.remove();
+            }, 3000);
+        }
+
+       
+    }
+})();
